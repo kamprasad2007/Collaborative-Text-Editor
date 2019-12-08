23de2bcb38bb0cb3e508d46ce48ac6cf4186c129
manoj <kamprasad2007@gmail.com>
2019-12-09 07:58:44 +0800

    code changes

--- dist/app.js
+++ dist/app.js
@@ -3,9 +3,8 @@
 var login = document.getElementById("login");
 var logout = document.getElementById("logout");
 
-if(isUserLoggedIn()){
-  location.href = "#/login";
-  login.style.visibility = null;
+if(!isUserLoggedIn()){
+  routePage();
 }
 
 var userSingedIn = false;
@@ -19,23 +18,18 @@ function onSignIn(googleUser) {
       Image : profile.getImageUrl(),
       Email : profile.getEmail()
     }
-    logout.style.visibility = null;;
-    login.style.visibility = "hidden";
     sessionStorage.setItem('user',JSON.stringify(user));
-    window.location = "#/page";
+    routePage()
   }else{
-    window.location = '#/login';
-    login.style.visibility = null;
+    routeLogin()
   }
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      logout.style.visibility = "hidden";
-      login.style.visibility = null;
       sessionStorage.removeItem('user');
-      location.hash = "#/login";
+      routeLogin()
     });
 }
 
@@ -46,9 +40,17 @@ function isUserLoggedIn(){
 window.addEventListener('hashchange', function(){
   if(isUserLoggedIn()){
     if(location.hash != "#/page"){
-      location.hash = "#/page";
+      routePage()
     }
   }else{
-      location.hash = "#/login";
+    routeLogin();
   }
 },false);
+
+function routeLogin(){
+  location.hash = "#/login";
+}
+
+function routePage(){
+  location.hash = "#/page";
+}
