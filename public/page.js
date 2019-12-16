9e587cf2081baf3ef727d915a02da5f0be69aa7a
manoj <kamprasad2007@gmail.com>
2019-12-16 21:52:06 +0800

    code change

--- public/page.js
+++ public/page.js
@@ -1,7 +1,8 @@
-var cm = CodeMirror.fromTextArea(document.getElementById('note'), {lineNumbers: false})
-cm.setSize(900, 300);
+
 var socket = io()
 socket.on('doc', function(data) {
+  var cm = CodeMirror.fromTextArea(document.getElementById('note'), {lineNumbers: false})
+  cm.setSize(900, 300);
   var serverAdapter = new ot.SocketIOAdapter(socket)
   var editorAdapter = new ot.CodeMirrorAdapter(cm)
   var client = new ot.EditorClient(data.revision, data.clients, serverAdapter, editorAdapter)
